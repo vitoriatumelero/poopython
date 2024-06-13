65d4a880943cd2258d8e8547a03d44c27235e38d
Vitória Tumelero <162608569+vitoriatumelero@users.noreply.github.com>
2024-06-13 13:55:40 -0300

    Delete musico.py

--- musico.py
+++
@@ -1,9 +0,0 @@
-from instrumento import Instrumento
-class Musico:
-    def __init__(self, nome, instrumento: Instrumento):
-       self.nome = nome
-       self.instrumento = instrumento
-    def mostrar_info(self):
-        print(f'Músico: {self.nome}')
-        self.instrumento.mostrar_info()
-    
